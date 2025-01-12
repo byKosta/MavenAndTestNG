8778eec6f642123548e2cfb98922dd2ad148708b
byKosta <haletskonstantsin@gmail.com>
2025-01-12 13:58:06 +0100

    Initial commit

---
+++ src/test/java/SaturdayTest.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
+public class SaturdayTest {
+    WebDriver driver = new ChromeDriver();
+
+    @BeforeMethod
+    public void baseURL() {
+        driver.get("https://www.umgdy.gov.pl/");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+
+    public void test() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        // Ожидание элемента <h1> с указанным текстом
+        WebElement h1Element = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//h1[text()='Urząd Morski w Gdyni']") // XPath для поиска элемента <h1> с нужным текстом
+        ));
+        // Получение текста из элемента
+        String actualText = h1Element.getText();
+        System.out.println("Found h1 text: " + actualText);
+
+        // Проверка текста
+        Assert.assertEquals(actualText, "Urząd Morski w Gdyni");
+    }
+    @Test
+    public void test1() throws InterruptedException {
+
+
+    }
+    @AfterTest
+            public void closeBrowser() {
+        driver.quit();
+    }
+
+}
