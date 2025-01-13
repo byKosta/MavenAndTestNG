6c7a6107e7999b38cd4a284b7dc3aa28a8102165
byKosta <haletskonstantsin@gmail.com>
2025-01-13 19:08:32 +0100

    added new test Login an account

--- src/test/java/runner/LoginTest.java
+++ src/test/java/runner/LoginTest.java
@@ -7,36 +7,59 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-
 public class LoginTest {
 
+    private WebDriver driver;
+    private WebDriverWait wait;
 
-    WebDriver driver;
-    WebDriverWait wait;
+    @BeforeMethod
+    public void setUp() {
+        // Инициализация драйвера
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        // Инициализация WebDriverWait
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
 
     @Test
     public void testLoginTitleVisibility() {
-        // Set up WebDriver and open the website
-        driver = new ChromeDriver();
-        driver.get("https://opensource-demo.orangehrmlive.com/");
-        driver.manage().window().maximize();
-
-        // Initialize WebDriverWait
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        // Find the <h5> element
+        // Проверка видимости заголовка "Login"
         WebElement h5Element = wait.until(ExpectedConditions.presenceOfElementLocated(
                 By.xpath("//h5[text()='Login']")));
-
-        // Validate that the element is displayed
         Assert.assertTrue(h5Element.isDisplayed(), "The 'Login' title is not visible!");
-
-        driver.quit();
     }
 
+    @Test
+    public void LoginInAccount() {
 
+        WebElement usernameField = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.name("username")));
+        usernameField.sendKeys("Admin");
+        WebElement passwordField = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.name("password")));
+        passwordField.sendKeys("admin123");
+
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//button[@type='submit']")));
+        loginButton.click();
+
+        WebElement profilePicture = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//img[@alt='profile picture']")));
+        Assert.assertTrue(profilePicture.isDisplayed(), "Profile picture is not displayed!");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        // Закрыть браузер после выполнения теста
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
