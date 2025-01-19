34dce55b3e9813cc9519f4c2b6b6d771a76b077a
Kanstantsin Halets <89216660+byKosta@users.noreply.github.com>
2025-01-19 16:38:41 +0100

    uodate Admin page test (#12)
    * add new templates for new tets
    
    * add new templates for new tets

--- src/test/java/runner/AdminPageTest.java
+++ src/test/java/runner/AdminPageTest.java
@@ -1,11 +1,32 @@
 package runner;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+
+import java.time.Duration;
+
 public class AdminPageTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+//    @BeforeMethod
+//    public void setUp() {
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//        driver.manage().window().maximize();
+//    }
 
     @Test
+
     public void adminPage(){
 
+//    @AfterMethod
+//    public void tearDown() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
     }
 }
