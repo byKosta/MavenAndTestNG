a2c2dd184b2e6fc799a97a5cc25cf13403be3c66
Kanstantsin Halets <89216660+byKosta@users.noreply.github.com>
2025-01-20 19:20:25 +0100

    Update Leave test (#15)

--- src/test/java/runner/LeavePageTest.java
+++ src/test/java/runner/LeavePageTest.java
@@ -1,11 +1,58 @@
 package runner;
 
-import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.time.Duration;
 
 public class LeavePageTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
 
+    @BeforeClass
+    public void setUp() {
+        // Инициализация драйвера
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        // Инициализация WebDriverWait
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void LoginInAccount() {
+
+        WebElement usernameField = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.name("username")));
+        usernameField.sendKeys("Admin");
+        WebElement passwordField = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.name("password")));
+        passwordField.sendKeys("admin123");
+
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//button[@type='submit']")));
+        loginButton.click();
+
+        WebElement profilePicture = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//img[@alt='profile picture']")));
+        Assert.assertTrue(profilePicture.isDisplayed(), "Profile picture is not displayed!");
+    }
     @Test
     public void leavepage(){
 
     }
-}
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    }
+
