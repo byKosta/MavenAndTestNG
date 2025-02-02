63646365c3ad3b3d2e7645e2c2b241ab30736231
Kanstantsin Halets <89216660+byKosta@users.noreply.github.com>
2025-02-02 18:28:54 +0100

    refactoring cucumber tests (#29)

--- src/test/java/cucumber/FreestyleJobSteps.java
+++ src/test/java/cucumber/FreestyleJobSteps.java
@@ -1,7 +1,7 @@
 package cucumber;
 
-
 import io.cucumber.java.en.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.*;
@@ -9,11 +9,13 @@ import org.testng.Assert;
 import java.time.Duration;
 
 public class FreestyleJobSteps {
+
     private WebDriver driver;
     private WebElement h1Element;
 
     @Given("I navigate to UMG Gdyni website")
     public void navigate_to_website() {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.umgdy.gov.pl/");
@@ -21,14 +23,18 @@ public class FreestyleJobSteps {
 
     @When("I wait for header element to load")
     public void wait_for_header() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        h1Element = wait.until(ExpectedConditions.presenceOfElementLocated(
-                By.xpath("//h1[text()='Urząd Morski w Gdyni']")));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        h1Element = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//h1[normalize-space()='Urząd Morski w Gdyni']")));
     }
 
     @Then("The header text should be {string}")
     public void verify_header_text(String expectedText) {
-        Assert.assertEquals(h1Element.getText(), expectedText);
+        Assert.assertEquals(
+                h1Element.getText().trim(),
+                expectedText,
+                "Текст заголовка не соответствует ожидаемому"
+        );
         driver.quit();
     }
 }
