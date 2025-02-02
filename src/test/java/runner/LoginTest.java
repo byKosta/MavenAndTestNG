fa1fed2c449b6d03788e44f1ed38837dd0218c9c
byKosta <haletskonstantsin@gmail.com>
2025-02-02 16:57:58 +0100

    refactoring pages.base

--- src/test/java/runner/LoginTest.java
+++ src/test/java/runner/LoginTest.java
@@ -7,7 +7,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import pages.LoginPage;
+import pages.base.LoginPage;
 
 public class LoginTest {
     private WebDriver driver;
