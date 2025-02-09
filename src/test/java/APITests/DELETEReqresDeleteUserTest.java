8712df387b1a2270e01d2d3d14eeef82933318e4
byKosta <haletskonstantsin@gmail.com>
2025-02-09 18:04:47 +0100

    refactoring Login page tests

--- src/test/java/APITests/DELETEReqresDeleteUserTest.java
+++ src/test/java/APITests/DELETEReqresDeleteUserTest.java
@@ -9,21 +9,21 @@ import static io.restassured.RestAssured.*;
 public class DELETEReqresDeleteUserTest {
     @Test
     public void deleteUserTest() {
-        // Устанавливаем базовый URL
+
         RestAssured.baseURI = "https://reqres.in";
 
-        // Выполняем DELETE-запрос
+
         Response response = given()
                 .when()
-                .delete("/api/users/2")                      // Указываем эндпоинт
+                .delete("/api/users/2")
                 .then()
-                .statusCode(204)                            // Проверяем, что статус код 204 (No Content)
-                .extract().response();                      // Извлекаем полный ответ
+                .statusCode(204)
+                .extract().response();
 
-        // Дополнительная проверка: тело ответа должно быть пустым
+
         Assert.assertEquals(response.asString().length(), 0, "Response body is not empty!");
 
-        // Логируем ответ
+
         System.out.println("Response status code: " + response.getStatusCode());
     }
 }
