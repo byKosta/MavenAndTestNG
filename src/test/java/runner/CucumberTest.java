ab4fa8a3ef715388c09680d07f6b0f8258440170
byKosta <haletskonstantsin@gmail.com>
2025-02-16 17:14:51 +0100

    refactoring cucumber tests

--- src/test/java/runner/CucumberTest.java
+++ src/test/java/runner/CucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/cucumber",
-        glue = {"cucumber","runner"}, // Исправленный путь к пакету с шагами
+        glue = {"cucumber","runner"},
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports.html",
